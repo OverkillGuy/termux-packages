d956e46e33f778c85549cd84f11bd5cfe0a8f43a
Jb DOYON <jb@jiby.tech>
2018-11-05 22:07:34 +0000

    Switch arch to arm64 for sbcl

--- packages/sbcl/build.sh
+++ packages/sbcl/build.sh
@@ -7,10 +7,10 @@ TERMUX_PKG_SRCURL=http://prdownloads.sourceforge.net/sbcl/sbcl-${TERMUX_PKG_VERS
 termux_step_pre_configure () {
 	local SBCL_HOST_TARFILE=$TERMUX_PKG_CACHEDIR/sbcl-host-${TERMUX_PKG_VERSION}.tar.bz2
 	if [ ! -f $SBCL_HOST_TARFILE ]; then
-		curl -o $SBCL_HOST_TARFILE -L http://downloads.sourceforge.net/project/sbcl/sbcl/${TERMUX_PKG_VERSION}/sbcl-${TERMUX_PKG_VERSION}-x86-64-linux-binary.tar.bz2
+		curl -o $SBCL_HOST_TARFILE -L http://downloads.sourceforge.net/project/sbcl/sbcl/${TERMUX_PKG_VERSION}/sbcl-${TERMUX_PKG_VERSION}-arm64-linux-binary.tar.bz2
 		cd $TERMUX_PKG_TMPDIR
 		tar xf $SBCL_HOST_TARFILE
-		cd sbcl-${TERMUX_PKG_VERSION}-x86-64-linux
+		cd sbcl-${TERMUX_PKG_VERSION}-arm64-linux
 		INSTALL_ROOT=$TERMUX_PKG_CACHEDIR/sbcl-host sh install.sh
 	fi
 	export PATH=$PATH:$TERMUX_PKG_CACHEDIR/sbcl-host/bin
@@ -19,5 +19,5 @@ termux_step_pre_configure () {
 
 termux_step_make_install () {
 	cd $TERMUX_PKG_SRCDIR
-	sh make.sh --prefix=$TERMUX_PREFIX
+	sh make.sh --prefix=$TERMUX_PREFIX --arch=arm64
 }
